5d4c233e35d39f797ae83ef983de77a6445849a4
Woong99 <dndry1072@naver.com>
2023-05-22 09:10:36 +0900

    first-commit

---
+++ src/test/kotlin/com/example/kotlinboard/KotlinBoardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinBoardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
